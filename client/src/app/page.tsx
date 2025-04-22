d50515eaaf9219c8f9a810f9ccef495e83bae73a
keyl <keildcastro@iskolarngbayan.pup.edu.ph>
2025-04-22 14:18:57 +0800

    W11-8 + initial commit for working test frontend

---
+++ client/src/app/page.tsx
@@ -0,0 +1,13 @@
+'use client';
+
+import { useState, /* useEffect */ } from 'react';
+
+export default function Home() {
+  const [message, /* setMessage */] = useState<string>('Loading...');
+
+  return (
+    <main>
+      <p>Message from backend: {message}</p>
+    </main>
+  );
+}
