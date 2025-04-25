8e000e759b80a84608fcd4494c1f317683038fd0
excess-ca <150656390+excess-ca@users.noreply.github.com>
2025-04-25 17:33:43 +0800

    W11-30 V1 + initial setup of sequelize

--- server/index.js
+++ server/index.js
@@ -21,3 +21,22 @@ try {
 } catch (error) {
   console.error(`Error occurred: ${error.message}`)
 }
+
+const Sequelize = require('sequelize')
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    dialect: 'postgres'
+  }
+)
+
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log('Database connection has been established successfully.')
+  })
+  .catch((error) => {
+    console.error('Unable to connect to the database:', error)
+  })
