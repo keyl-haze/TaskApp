5fc0fbe6a3aaf32c8bf25712723831ff0a698338
keyl <keildcastro@iskolarngbayan.pup.edu.ph>
2025-04-25 13:52:49 +0800

    W11-14 V2 + frontend eslint and prettier setup and initial config

--- client/eslint.config.mjs
+++ client/eslint.config.mjs
@@ -1,16 +1,56 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { dirname } from 'path'
+import { fileURLToPath } from 'url'
+import { FlatCompat } from '@eslint/eslintrc'
+import js from '@eslint/js'
+import globals from 'globals'
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
 
 const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
+  baseDirectory: __dirname
+})
 
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-];
+export default [
+  ...compat.extends('next/core-web-vitals', 'next/typescript'),
 
-export default eslintConfig;
+  {
+    files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'],
+    plugins: {
+      js
+    },
+    languageOptions: {
+      globals: globals.browser
+    },
+    rules: {
+      // * no-console rule
+      'no-console': [
+        'warn',
+        {
+          allow: ['warn', 'error']
+        }
+      ],
+
+      // * no-unused-vars rule
+      'no-unused-vars': [
+        'warn',
+        {
+          vars: 'all',
+          args: 'after-used',
+          caughtErrors: 'all',
+          ignoreRestSiblings: true,
+          reportUsedIgnorePattern: false
+        }
+      ],
+
+      // * require-default-props rule
+      'react/require-default-props': [
+        'warn',
+        {
+          forbidDefaultForRequired: true,
+          ignoreFunctionalComponents: true
+        }
+      ]
+    }
+  }
+]
