6998da6f2fff2451cae5b0483617584b74bb4376
wizy.ejayazon <wizy.ejayazon@gmail.com>
2025-06-04 19:04:34 +0800

    * fixed users file naming

--- server/routes/api/v1/users.js
+++ server/routes/api/v1/users.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = new express.Router();
-const controller = require(`${__serverRoot}/api/v1/user/controller`);
+const controller = require(`${__serverRoot}/api/v1/users/controller`);
+
 router.get('/', controller.list);
 router.get('/:id', controller.get);
 router.post('/', controller.create);
@@ -8,4 +9,5 @@ router.put('/:id', controller.update);
 router.patch('/:id/restore', controller.restore);
 router.patch('/:id', controller.update);
 router.delete('/:id', controller.remove);
+
 module.exports = router;
