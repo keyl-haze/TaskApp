5e2c52ee9421534477823149c919dfc9838d0545
wizy.ejayazon <wizy.ejayazon@gmail.com>
2025-06-05 02:06:46 +0800

    * made the userprojects unique

--- server/migrations/20250603034812-create-project-user.js
+++ server/migrations/20250603034812-create-project-user.js
@@ -2,30 +2,49 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('ProjectUsers', {
-      id: {
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-        type: Sequelize.INTEGER
-      },
-      userId: {
-        allowNull: false,
-        references: {
-          key: 'id',
-          model: 'Users'
+    await queryInterface.createTable(
+      'ProjectUsers',
+      {
+        id: {
+          allowNull: false,
+          autoIncrement: true,
+          primaryKey: true,
+          type: Sequelize.INTEGER
         },
-        type: Sequelize.INTEGER
-      },
-      projectId: {
-        allowNull: false,
-        references: {
-          key: 'id',
-          model: 'Projects'
+        userId: {
+          allowNull: false,
+          references: {
+            key: 'id',
+            model: 'Users'
+          },
+          type: Sequelize.INTEGER
         },
-        type: Sequelize.INTEGER
+        projectId: {
+          allowNull: false,
+          references: {
+            key: 'id',
+            model: 'Projects'
+          },
+          type: Sequelize.INTEGER
+        },
+        createdAt: {
+          allowNull: false,
+          type: Sequelize.DATE
+        },
+        updatedAt: {
+          allowNull: false,
+          type: Sequelize.DATE
+        }
+      },
+      {
+        uniqueKeys: {
+          uniqueProjectUser: {
+            customIndex: true,
+            fields: ['userId', 'projectId']
+          }
+        }
       }
-    });
+    );
   },
   async down(queryInterface, Sequelize) {
     await queryInterface.dropTable('ProjectUsers');
